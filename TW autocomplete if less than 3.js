687cc67749d9359983e9e4f9f836fb95f68e5c9f
Meiogordo <meio.gordo@hotmail.com>
2016-12-13 00:40:59 +0000

    Autocompleter if less than 3 added

---
+++ TW autocomplete if less than 3.js
@@ -0,0 +1,18 @@
+javascript:
+
+var comparison = "0:02:59";
+
+function clicaNessaBeep(){
+	
+	var currentTime = document.getElementsByClassName('nowrap lit-item')[0].textContent.trim();
+
+	if(currentTime < comparison){
+
+		document.getElementsByClassName('order_feature btn btn-btr btn-instant-free')[0].click();
+		//risky atm, I'll test with alerts - nah screw it
+
+	}
+
+}
+
+setInterval(clicaNessaBeep, 3000); //cuidado com este valor, é em ms, n pôr baixo demais
