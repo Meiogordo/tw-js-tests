49c7bf3b9fcbd5a6d32004f3cbcce9999e41f6ec
Meiogordo <Meiogordo@users.noreply.github.com>
2016-12-14 12:33:36 +0000

    Added comments to Cls Report cenas

--- TW Cls from report with time.js
+++ TW Cls from report with time.js
@@ -1,2 +1,4 @@
 var got = document.getElementsByClassName('small grey')[0].parentElement.textContent.trim()
-//
+//this will get the time from the report - still have to parse it
+//Date example: "14/Dez/2016 (11:30:09):008"
+//Date under format: day/monthText/year (hour:min:second):milisecond
