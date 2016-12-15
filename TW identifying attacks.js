a76ed664f052c02d94d26dcb160491f7274510c3
Meiogordo <meio.gordo@hotmail.com>
2016-12-15 18:38:30 +0000

    Switched identifying attacks village name get to jQuery

--- TW identifying attacks.js
+++ TW identifying attacks.js
@@ -7,10 +7,14 @@ vizold = table.getElementsByClassName('vis');
 
 
 //same as above but simplified
-viz = document.getElementById('content_value').getElementsByClassName('vis');
+//viz = document.getElementById('content_value').getElementsByClassName('vis');
+//o viz é 100% unnecessary
 
 
+//textoaldeia1 = document.getElementsByClassName('village_anchor contexted')[0].outerText; //aldeia 1
+//textoaldeia2 = document.getElementsByClassName('village_anchor contexted')[1].outerText; //aldeia 2
 
-textoaldeia1 = viz[0].getElementsByClassName('village_anchor contexted')[0].outerText; //aldeia 1
-textoaldeia2 = viz[0].getElementsByClassName('village_anchor contexted')[1].outerText; //aldeia 2
+//agora em jQuery, o resto deu byebye
+var textoaldeia1 = $(".village_anchor.contexted")[0].outerText;
+var textoaldeia2 = $(".village_anchor.contexted")[1].outerText;
 
