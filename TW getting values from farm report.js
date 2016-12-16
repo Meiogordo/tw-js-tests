2d6c140ebab6ce5bd3069bd3eeb140d202eed9c2
Meiogordo <meio.gordo@hotmail.com>
2016-12-16 11:31:04 +0000

    Updated report resource getter to deal with values over 999

--- TW getting values from farm report.js
+++ TW getting values from farm report.js
@@ -13,8 +13,20 @@ if (spy == null){
 	var out2 = 0;
 	for (i=0;i<spy.getElementsByClassName('nowrap').length;i++) {
 
-		output += spy.getElementsByClassName('nowrap')[i].children[0].title + ': ' + spy.getElementsByClassName('nowrap')[i].textContent + "\n"
-		out2 += parseInt(spy.getElementsByClassName('nowrap')[i].textContent);
+		//output += spy.getElementsByClassName('nowrap')[i].children[0].title + ': ' + spy.getElementsByClassName('nowrap')[i].textContent + "\n"
+		//out2 += parseInt(spy.getElementsByClassName('nowrap')[i].textContent);
+
+		//testar para lidar com 1.x (maiores que 1000)
+
+		if(spy.getElementsByClassName('nowrap')[i].textContent.indexOf('.') == -1){
+			//caso base - same as before
+			out2 += parseInt(spy.getElementsByClassName('nowrap')[i].textContent);
+
+		}else{
+
+			out2 += parseInt(spy.getElementsByClassName('nowrap')[i].textContent.trim().substr(0,1)) * 1000 + parseInt(spy.getElementsByClassName('nowrap')[i].textContent.trim().substring(2));
+
+		}
 
 	}
 
