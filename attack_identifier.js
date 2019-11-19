af89b516c4a424718fe2e037988064d4a85127a6
miguelpduarte <miguelpduarte98@gmail.com>
2019-11-19 01:02:12 +0000

    Make attack identifier reusable with several includes

--- attack_identifier.js
+++ attack_identifier.js
@@ -1,22 +1,22 @@
-//// Helper definitions
-const troopTimes = [18, 22, 9, 10, 11, 30, 35];
-const troopNames = ["Lanceiros, Vikings ou Arqueiros", "Espadachins", "Batedores", "Cavalaria Leve, Arqueiros a Cavalo ou Paladino", "Cavalaria Pesada", "Aríetes ou Catapultas", "Nobre"];
+(() => {
+	//// Helper definitions
+	const troopTimes = [18, 22, 9, 10, 11, 30, 35];
+	const troopNames = ["Lanceiros, Vikings ou Arqueiros", "Espadachins", "Batedores", "Cavalaria Leve, Arqueiros a Cavalo ou Paladino", "Cavalaria Pesada", "Aríetes ou Catapultas", "Nobre"];
 
-const getVillagesFullNames = () => [...document.querySelectorAll(".village_anchor.contexted")].map(node => node.textContent);
+	const getVillagesFullNames = () => [...document.querySelectorAll(".village_anchor.contexted")].map(node => node.textContent);
 
-const village_full_name_coords_regex = /\((...)\|(...)\) K..$/;
-const villageFullNameToCoords = (village_full_name) => village_full_name_coords_regex.exec(village_full_name);
+	const village_full_name_coords_regex = /\((...)\|(...)\) K..$/;
+	const villageFullNameToCoords = (village_full_name) => village_full_name_coords_regex.exec(village_full_name);
 
-const distanceFromCoords = ([x1, y1], [x2, y2]) => Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	const distanceFromCoords = ([x1, y1], [x2, y2]) => Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 
-// Returns a String formatted as HH:MM:SS
-const timeFromDistanceAndUnitTime = (distance) => (unit_time_mins) => {
-	//minutes -> seconds to print in hh:mm:ss using Date object
-	return new Date(distance * unit_time_mins * 60 * 1000).toISOString().substr(11, 8);
-}
+	// Returns a String formatted as HH:MM:SS
+	const timeFromDistanceAndUnitTime = (distance) => (unit_time_mins) => {
+		//minutes -> seconds to print in hh:mm:ss using Date object
+		return new Date(distance * unit_time_mins * 60 * 1000).toISOString().substr(11, 8);
+	}
 
-//// Program start
-const doIt = () => {
+	//// Program start
 	// village 1 - attack origin
 	// village 2 - attack destination
 
@@ -27,6 +27,8 @@ const doIt = () => {
 	const [__, village2_x, village2_y] = villageFullNameToCoords(village2);
 	const distance = distanceFromCoords([village1_x, village1_y], [village2_x, village2_y]);
 	const timeFromUnitTime = timeFromDistanceAndUnitTime(distance);
+
+	// TODO: Make myself prouder of this by using map/reduce/both
 	const times = {};
 	for (let i = 0; i < troopTimes.length; ++i) {
 		times[troopNames[i]] = timeFromUnitTime(troopTimes[i]);
@@ -34,9 +36,7 @@ const doIt = () => {
 
 	console.log("distance", distance);
 	console.log("times", times);
-};
-
-doIt();
+})();
 
 //exemplo para analisar dps:
 //(451|375) (450|372) tem distance 3,16 campos
